b8ce61aad08658f417cbfce197790a35b4379cda
matheus-frota <matheusfrota@hotmail.com.br>
2022-04-12 03:20:30 -0300

    feat: Adição de novas bibliotecas e mudança no arquivo resultante da migração.

--- src/roit-api/migrations/models/0_20220412051706_init.sql
+++ src/roit-api/migrations/models/0_20220412051706_init.sql
@@ -7,7 +7,8 @@ CREATE TABLE IF NOT EXISTS "empresa" (
     "cod_qualificacao_do_responsavel" TEXT,
     "capital_social" TEXT,
     "cod_porte_da_empresa" TEXT,
-    "desc_ente_federativo_responsavel" TEXT
+    "desc_ente_federativo_responsavel" TEXT,
+    "created_at" DATE NOT NULL
 );
 CREATE TABLE IF NOT EXISTS "aerich" (
     "id" SERIAL NOT NULL PRIMARY KEY,
